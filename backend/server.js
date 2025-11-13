174e6e5c1fc1cbe9f14f3e7d600a4690112721b6
fahadakram2 <fr1727167@gmail.com>
2025-11-13 11:10:24 +0500

    change cors

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*", // allow all origins (not recommended for production)
+  })
+);
 
 // simple health route for EB to test
 app.get("/", (req, res) => {
